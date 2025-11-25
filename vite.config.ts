e4e87e1d01cc9d629b01abc1d016ac57adbc1b11
Aliyah Crowe <awi6295@wgu.edu>
2025-11-25 16:19:08 -0500

    idk anymore

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Flow_Finance/', // GitHub Pages repository name
+  base: '/budget-bestie/', // GitHub Pages repository name
 })
